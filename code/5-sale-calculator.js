d7a715b0a5e5c96570f5126e3854ddb4557b2090
Azu Chan <Kogaaz68@craighead.school.nz>
2024-07-31 02:56:25 +0000

    yunaloveyuna

--- code/5-sale-calculator.js
+++ code/5-sale-calculator.js
@@ -5,6 +5,12 @@
 function saleCalculator() {
   let money = ('How much money do you have to spend ?')
   let item = ('How much is the item normally ?')
-  let percentage = ('And what is the discount pecentage ?')
-  
+  let discount = ('And what is the discount pecentage ?')
+  let percentage = discount * 0.01
+  let one = 1 - percentage
+  let cost = item * one
+  if (cost > money) {
+  alert("You don't have enough. You have $" + money+ ' to spend, but even with ' + discount + '% off, $' + item +' only comes down to $' + cost +'.')
+  } else {
+  alert ("You have enough. You have $" + money + ' to spend, it is less than ' + discount + '% off, $' + item + 'only comes down to $'+ cost + '.')
 }
